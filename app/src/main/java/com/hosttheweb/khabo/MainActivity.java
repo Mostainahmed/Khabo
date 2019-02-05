c78d92485e00c3ed1e4f7a31a6ce63b78aeb4cdd
Mostainahmed <mostainahmed@gmail.com>
2019-02-05 17:03:38 +0600

    Second Commit

--- app/src/main/java/com/hosttheweb/khabo/MainActivity.java
+++ app/src/main/java/com/hosttheweb/khabo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.hosttheweb.khabo;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //for Full Screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        //This thread will automatically destroy this activity after "SPLASH_TIME_OUT" seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
